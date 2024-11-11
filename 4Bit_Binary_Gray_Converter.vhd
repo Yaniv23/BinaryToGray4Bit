059f9d075035396bb4924079de8f451b41ecfd6b
Yaniv23 <94038703+Yaniv23@users.noreply.github.com>
2024-11-11 20:54:04 +0200

    Update 4Bit_Binary_Gray_Converter.vhd

--- 4Bit_Binary_Gray_Converter.vhd
+++ 4Bit_Binary_Gray_Converter.vhd
@@ -1,35 +1,5 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 05.11.2024 19:15:58
--- Design Name: 
--- Module Name: 4Bit_Binary_Gray_Converter - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 library ieee;
 use ieee.std_logic_1164.all;
